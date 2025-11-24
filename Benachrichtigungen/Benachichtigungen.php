308adf2a9c0671e530c943ab395182cafead4a67
gregor-thb <gregor.druse@th-brandenburg.de>
2025-11-24 15:12:18 +0100

    Entwurf Popup

---
+++ Benachrichtigungen/Benachichtigungen.php
@@ -0,0 +1,19 @@
+<head>
+
+<meta charset="UTF-8">
+
+<title>Benachrichtigungen</title>
+
+<link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+
+<div class="container">
+
+<button type="submit" class="btn"
+oneclick="openPopup()">Senden</button>
+
+<div class="popup" id="popup">
+
+<img src="
